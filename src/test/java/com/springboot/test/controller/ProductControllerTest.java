7f4ccde0bebcd1a3a93e79c42ba71690eef508fb
qnmgjeklnbrl <rns0721@naver.com>
2023-09-24 09:50:16 +0900

    controller, service 레이어 테스트 코드

--- src/test/java/com/springboot/test/controller/ProductControllerTest.java
+++ src/test/java/com/springboot/test/controller/ProductControllerTest.java
@@ -1,6 +1,6 @@
 package com.springboot.test.controller;
 
-import com.google.gson.Gson;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
+
+import com.google.gson.Gson;
 import com.springboot.test.data.dto.ProductDto;
 import com.springboot.test.data.dto.ProductResponseDto;
 import com.springboot.test.service.impl.ProductserviceImpl;
@@ -16,7 +18,7 @@ import com.springboot.test.service.impl.ProductserviceImpl;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;;
@@ -62,7 +64,20 @@ public class ProductControllerTest {
             .stock(2000)
             .build();
 
-        Gson gson
-    }
+        Gson gson = new Gson();
+        String content = gson.toJson(productDto);
+
+        mockMvc.perform(
+                    post("/product")
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.number").exists())
+                .andExpect(jsonPath("$.name").exists())
+                .andExpect(jsonPath("$.price").exists())
+                .andExpect(jsonPath("$.stock").exists())
+                .andDo(print());
+        verify(productservice).saveProduct(new ProductDto("pen", 5000, 2000));
+    }   
 
 }
